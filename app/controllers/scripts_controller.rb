277689a80b7dc375eabae2b60b32814adfa1608f
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-08-06 11:56:34 -0400

    set up index, show pages for controllers

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,7 +1,29 @@
 class ScriptsController < ApplicationController
 
   def index
-      @scripts = Script.all
+    @scripts = Script.all
+  end
+
+  def show
+    @script = Script.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def new
+    @script = Script.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+  def script_params
+    params.require(:script).permit(:quote, :character, :show)
   end
 
 end
